8ee72fe14b167283675a1267683904009c10e365
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-04 12:00:22 -0500

    PersonTest-testConstructor

--- lambdas/src/test/java/PersonTest.java
+++ lambdas/src/test/java/PersonTest.java
@@ -1,10 +1,35 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class PersonTest {
 
     @Test
     public void testConstructor() {
+        // Given
+        String name = "Stella";
+        LocalDate birthday = LocalDate.MAX;
+        Person.Sex gender = Person.Sex.FEMALE;
+        String emailAddress = "stella@gmail.com";
 
+        Person person = new PersonBuilder()
+                .setName(name)
+                .setBirthday(birthday)
+                .setGender(gender)
+                .setEmailAddress(emailAddress)
+                .createPerson();
+
+        // When
+        String actualName = person.getName();
+        LocalDate actualBirthday = person.getBirthday();
+        Person.Sex actualGender = person.getGender();
+        String actualEmailAddress = person.getEmailAddress();
+
+        // Then
+        Assert.assertEquals(name, actualName);
+        Assert.assertEquals(birthday, actualBirthday);
+        Assert.assertEquals(gender, actualGender);
+        Assert.assertEquals(emailAddress, actualEmailAddress);
     }
 }

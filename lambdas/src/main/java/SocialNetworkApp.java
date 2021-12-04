2b0c6094b6e66bd07c681be63f279f2114c35c32
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-04 12:48:20 -0500

    removed approaches 1 & 2 from SocialNetworkApp

--- lambdas/src/main/java/SocialNetworkApp.java
+++ lambdas/src/main/java/SocialNetworkApp.java
@@ -24,21 +24,4 @@ public class SocialNetworkApp {
         return this.roster.size();
     }
 
-    // Approach 1 - match one (age)
-    public static void printPersonsOlderThan(List<Person> roster, int age) {
-        for (Person p : roster) {
-            if (p.getAge() >= age) {
-                p.printPerson();
-            }
-        }
-    }
-
-    // Approach 2 - general search
-    public static void printPersonsWithinAgeRange(List<Person> roster, int low, int high) {
-        for (Person p : roster) {
-            if (low <= p.getAge() && p.getAge() < high) {
-                p.printPerson();
-            }
-        }
-    }
 }
--- lambdas/src/test/java/SocialNetworkAppTest.java
+++ lambdas/src/test/java/SocialNetworkAppTest.java
@@ -1,16 +1,50 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class SocialNetworkAppTest {
+
+    private Person bob = new PersonBuilder()
+            .setName("Bob")
+            .setEmailAddress("bob@email.com")
+            .setGender(Person.Sex.MALE)
+            .setBirthday(LocalDate.of(1990, 1, 1))
+            .createPerson();
+
+    private Person michelle = new PersonBuilder()
+            .setName("Michelle")
+            .setEmailAddress("michelle@email.com")
+            .setGender(Person.Sex.FEMALE)
+            .setBirthday(LocalDate.of(1995, 1, 1))
+            .createPerson();
+
+    private Person angel = new PersonBuilder()
+            .setName("Angel")
+            .setBirthday(LocalDate.of(2000, 1, 1))
+            .setGender(Person.Sex.FEMALE)
+            .setEmailAddress("angel@email.com")
+            .createPerson();
+
+    private Person dominic = new PersonBuilder()
+            .setBirthday(LocalDate.of(2010, 1, 1))
+            .setName("Dominic")
+            .setGender(Person.Sex.MALE)
+            .setEmailAddress("dominic@email.com")
+            .createPerson();
+
+    private Person tim = new PersonBuilder()
+            .setBirthday(LocalDate.of(2015, 1, 1))
+            .setName("Tim")
+            .setGender(Person.Sex.MALE)
+            .setEmailAddress("tim@email.com")
+            .createPerson();
 
     @Test
     public void testConstructor() {
         // Given
         SocialNetworkApp app = new SocialNetworkApp();
         Integer expected = 1;
-        Person bob = new PersonBuilder()
-                .setName("Bob")
-                .createPerson();
 
         // When
         app.addToRoster(bob);
@@ -19,4 +53,6 @@ public class SocialNetworkAppTest {
         // Then
         Assert.assertEquals(expected, actual);
     }
+
+
 }

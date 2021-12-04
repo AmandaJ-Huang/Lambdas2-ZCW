538cb1a54fc68fcaadafbf550fd888289983c78d
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-04 13:58:24 -0500

    set up main with populated SocialNetworkApp instance

--- lambdas/src/main/java/Main.java
+++ lambdas/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -7,5 +9,46 @@ public class Main {
                 p.printPerson();
             }
         }
+    }
+
+    public static void main(String[] args) {
+        // People
+        Person bob = new PersonBuilder().setName("Bob")
+                .setEmailAddress("bob@email.com")
+                .setGender(Person.Sex.MALE)
+                .setBirthday(LocalDate.of(1990, 1, 1))
+                .createPerson();
+
+        Person michelle = new PersonBuilder()
+                .setName("Michelle")
+                .setEmailAddress("michelle@email.com")
+                .setGender(Person.Sex.FEMALE)
+                .setBirthday(LocalDate.of(1995, 1, 1))
+                .createPerson();
+
+        Person angel = new PersonBuilder()
+                .setName("Angel")
+                .setBirthday(LocalDate.of(2000, 1, 1))
+                .setGender(Person.Sex.FEMALE)
+                .setEmailAddress("angel@email.com")
+                .createPerson();
+
+        Person dominic = new PersonBuilder()
+                .setBirthday(LocalDate.of(2010, 1, 1))
+                .setName("Dominic")
+                .setGender(Person.Sex.MALE)
+                .setEmailAddress("dominic@email.com")
+                .createPerson();
+
+        Person tim = new PersonBuilder()
+                .setBirthday(LocalDate.of(2015, 1, 1))
+                .setName("Tim")
+                .setGender(Person.Sex.MALE)
+                .setEmailAddress("tim@email.com")
+                .createPerson();
+
+        // SocialNetwork
+        List<Person> roster = Arrays.asList(bob, michelle, angel, dominic, tim);
+        SocialNetworkApp socialNetworkApp = new SocialNetworkApp(roster);
     }
 }

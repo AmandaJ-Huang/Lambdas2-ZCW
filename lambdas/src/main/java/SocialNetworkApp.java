0b01b182d59d7cf9444275723789e1fcb6a2869e
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-04 12:22:36 -0500

    Created given methods (approach 1 & 2); SocialNetworkAppTest

--- lambdas/src/main/java/SocialNetworkApp.java
+++ lambdas/src/main/java/SocialNetworkApp.java
@@ -11,4 +11,22 @@ public class SocialNetworkApp {
     public SocialNetworkApp(List<Person> roster) {
         this.roster = roster;
     }
+
+    // Approach 1 - match one (age)
+    public static void printPersonsOlderThan(List<Person> roster, int age) {
+        for (Person p : roster) {
+            if (p.getAge() >= age) {
+                p.printPerson();
+            }
+        }
+    }
+
+    // Approach 2 - general search
+    public static void printPersonsWithinAgeRange(List<Person> roster, int low, int high) {
+        for (Person p : roster) {
+            if (low <= p.getAge() && p.getAge() < high) {
+                p.printPerson();
+            }
+        }
+    }
 }
---
+++ lambdas/src/test/java/SocialNetworkAppTest.java
@@ -0,0 +1,2 @@
+public class SocialNetworkAppTest {
+}
